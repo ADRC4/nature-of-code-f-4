ac870ba1f5ab74997322a91d50a5bec632193d21
ChenL <34891235+nicolecshine@users.noreply.github.com>
2018-01-07 12:18:48 +0000

    Add files via upload

---
+++ Assets/Chapter2.9/Attraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attraction : MonoBehaviour
+{
+    public Transform BigTarget;
+    public Transform SmallTarget;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        GameObject Big = GameObject.Find("Big");
+        BigTarget = Big.transform;
+
+    }
+
+    void Awake()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        SmallTarget = transform;
+        rb.AddForce(transform.forward * 100, ForceMode2D.Force);
+        rb.AddForce(transform.up * 100, ForceMode2D.Force);
+
+    }
+
+
+
+    void Update()
+    {
+        Vector3 line = BigTarget.position - SmallTarget.position;
+        line.Normalize();
+
+        float distance = Vector3.Distance(BigTarget.position, SmallTarget.position);
+        rb.AddForce(line * 10 / distance);
+
+
+    }
+
+
+}
