73185c5a36dbaffaa032f006947965c29d3dfe3b
yjhZhou <34634244+yjhZhou@users.noreply.github.com>
2018-01-08 23:09:24 +0100

    Shakespeare's phrases
    Exercise9.7 use DNA script in Exercise 9_2-9_6

---
+++ Assets/Chapter9/Exercise9.7/Exponentialpopulation.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Exponentialpopulation
+{
+    float mutationRate;           // Mutation rate
+    DNA[] population;             // Array to hold the current population
+    List<DNA> matingPool;    // ArrayList which we will use for our "mating pool"
+    string target;                // Target phrase
+    int generations;              // Number of generations
+    public bool finished ;             // Are we finished evolving?
+    int perfectScore;
+
+    public Exponentialpopulation(string p, float m, int num)
+    {
+        target = p;
+        mutationRate = m;
+        population = new DNA[num];
+        for (int i = 0; i < population.Length; i++)
+        {
+            population[i] = new DNA(target.Length);
+        }
+        calcFitness();
+        matingPool = new List<DNA>();
+        finished = false;
+        generations = 0;
+
+        perfectScore = 1;
+        //perfectScore = (int)(Mathf.Pow(2, target.Length));
+    }
+    public void calcFitness()
+    {
+        for (int i = 0; i < population.Length; i++)
+        {
+            population[i].Exponentialfitness(target);
+        }
+    }
+    public void naturalSelection()
+    {
+        // Clear the ArrayList
+        matingPool.Clear();
+
+        float maxFitness = 0;
+        for (int i = 0; i < population.Length; i++)
+        {
+            if (population[i].fitness1 > maxFitness)
+            {
+                maxFitness = population[i].fitness1;
+            }
+        }
+
+        for (int i = 0; i < population.Length; i++)
+        {
+
+            // float fitness = map(population[i].fitness, 0, maxFitness, 0, 1);
+            float fitness = population[i].fitness1 / maxFitness;
+            int n = (int)(fitness * 100);  
+            // Arbitrary multiplier, we can also use monte carlo method
+            for (int j = 0; j < n; j++)
+            {           
+                // and pick two random numbers
+                matingPool.Add(population[i]);
+            }
+            
+        }
+        
+    }
+    public void generate()
+    {
+        // Refill the population with children from the mating pool
+        for (int i = 0; i < population.Length; i++)
+        {
+            int a = (int)(Random.Range(0, matingPool.Count));
+            int b = (int)(Random.Range(0, matingPool.Count));
+
+            //exercise9.4 two unique 'parents'
+            if (a == b)
+                b = b + (b > 0.5 * matingPool.Count ? -1 : 1);
+
+
+            DNA partnerA = matingPool[a];
+            DNA partnerB = matingPool[b];
+            DNA child = partnerA.crossover(partnerB);
+            child.mutate(mutationRate);
+            population[i] = child;
+        }
+        generations++;
+    }
+
+    public string getBest()
+    {
+        float worldrecord = 0.0f;
+        int index = 0;
+        for (int i = 0; i < population.Length; i++)
+        {
+            if (population[i].fitness1 > worldrecord)
+            {
+                index = i;
+                worldrecord = population[i].fitness1;
+            }
+        }
+
+        if (worldrecord == perfectScore)
+            finished = true;
+        return population[index].getPhrase();
+    }
+
+    public bool Finished()
+    {
+        return finished;
+    }
+
+    public int getGenerations()
+    {
+        return generations;
+    }
+
+    public float getAverageFitness()
+    {
+        float total = 0;
+        for (int i = 0; i < population.Length; i++)
+        {
+            total += population[i].fitness1;
+        }
+        return total / (population.Length);
+    }
+    public string allPhrases()
+    {
+        string everything = "";
+
+        int displayLimit = Mathf.Min(population.Length, 50);
+
+
+        for (int i = 0; i < displayLimit; i++)
+        {
+            everything += population[i].getPhrase() + "\n";
+        }
+        return everything;
+    }
+}
---
+++ Assets/Chapter9/ShakespeareExercise9_2-9_6/DNA/DNA.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA 
+{
+    
+    // The genetic sequence
+    char[] genes;
+
+    public float fitness;
+    public float fitness1;
+
+    // Constructor (makes a random DNA)
+    public DNA(int num)
+    {
+        genes = new char[num];
+        for (int i = 0; i < genes.Length; i++)
+        {
+            genes[i] = (char)Random.Range(32, 128);  // Pick from range of chars
+        }
+    }
+
+    // Converts character array to a String
+    public string getPhrase()
+    {
+        return new string(genes);
+    }
+
+    // Fitness function (returns floating point % of "correct" characters)
+    public void Fitness(string target)
+    {
+        int score = 0;
+        for (int i = 0; i < genes.Length; i++)
+        {
+            if (genes[i] == target[i])
+            {
+                score++;
+            }
+        }
+
+
+        fitness = (float)score / (float)target.Length;
+    }
+    public void Exponentialfitness(string target)
+    {
+        int score = 0;
+        for (int i = 0; i < genes.Length; i++)
+        {
+            if (genes[i] == target[i])
+            {
+                score++;
+            }
+        }
+        //fitness1 = (float)score / (float)target.Length;
+        fitness1 = Mathf.Pow(2, score)/Mathf.Pow(2,target.Length);
+        //fitness1 = Mathf.Pow(2, score);
+        //fitness1 = Mathf.Pow(score, 2);
+
+    }
+
+    // Crossover
+    public DNA crossover(DNA partner)
+    {
+        // A new child
+        DNA child = new DNA(genes.Length);
+
+        int midpoint = (int)(Random.Range(0,genes.Length)); // Pick a midpoint
+
+        // Half from one, half from the other
+        for (int i = 0; i < genes.Length; i++)
+        {
+            if (i > midpoint) child.genes[i] = genes[i];
+            else child.genes[i] = partner.genes[i];
+        }
+        return child;
+    }
+    // Based on a mutation probability, picks a new random character
+    public void mutate(float mutationRate)
+    {
+        for (int i = 0; i < genes.Length; i++)
+        {
+            if (Random.value < mutationRate)
+            {
+                genes[i] = (char)Random.Range(32, 128);
+            }
+        }
+    }
+   
+}
